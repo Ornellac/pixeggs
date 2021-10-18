97a665f38826987bd23c3828f5fa1851213e251d
ornella <cuttaia.ornella@gmail.com>
2021-10-18 10:03:47 +0200

    ModelMonstre

--- src/app/models/monster.ts
+++ src/app/models/monster.ts
@@ -1,7 +1,5 @@
 export interface Monster{
-    PK_Monster_ID:number;
-    Nom:string;
-    FK_Rarete_ID:number;
-    FK_Oeuf_ID:number;
-    Apparence:string;
+    PK_Monster:number;
+    NomMonstre:string;
+    ApparenceMonstre:string;
 }
