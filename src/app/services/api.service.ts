8a133a88235a7ab39f0899b3ebda8dac89fdf7b7
ornella <cuttaia.ornella@gmail.com>
2021-10-21 09:52:55 +0200

    test

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,6 +4,7 @@ import { Monstre } from '../models/monstre';
 import { Utilisateur } from '../models/utilisateur';
 import { Stat } from '../models/stat';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,19 @@ export class ApiService {
   private _url:string = 'http://localhost:3000'
 
   constructor(private _http: HttpClient) { }
+
+  // //! TEST:
+  // getTypeRequest() {
+  //   return this._http.get(`${this._url}/utilisateur`).pipe(map(res => {
+  //   return res;
+  //   }));
+  // }
+  // postTypeRequest() {
+  //   return this._http.post(`${this._url}/utilisateur`).pipe(map(res => {
+  //   return res;
+  //   }));
+  // }
+  // //! FIN TEST;
 
   //* Les Monstres :
   public getMonstre(): Observable<any>{
